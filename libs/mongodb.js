7610847277649b880c25507e6f936a79018c8345
Dechawat Royda <114420068+DechawatRoyda@users.noreply.github.com>
2024-04-25 21:30:23 +0700

    mongo

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -1,13 +1,39 @@
-import  mongoose  from "mongoose";
+// import  mongoose  from "mongoose";
 
-const connectMongoDB = async () =>{
-    try {
-        await mongoose.connect(process.env.MONGODB_URL);
-        console.log("Connected to MongoDB.");
-    } catch (error) {
-        console.log(error);
+// const connectMongoDB = async () =>{
+//     try {
+//         await mongoose.connect(process.env.MONGODB_URL);
+//         console.log("Connected to MongoDB.");
+//     } catch (error) {
+//         console.log(error);
         
-    }
-};
+//     }
+// };
 
-export default connectMongoDB;
+// export default connectMongoDB;
+import mongoose from 'mongoose';
+
+let isConnected = false; // track the connection
+
+export const connectMongoDB = async () => {
+  mongoose.set('strictQuery', true);
+
+  if(isConnected) {
+    console.log('MongoDB is already connected');
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+
+    isConnected = true;
+
+    console.log('MongoDB connected')
+  } catch (error) {
+    console.log(error);
+  }
+}  
